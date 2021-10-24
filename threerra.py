b54913399489960be50b34b76cf93387afb854c3
Jorge Gidi <jorgegidi@gmail.com>
2021-10-24 14:01:10 -0300

    add calibrate_pi_amp_12

--- threerra.py
+++ threerra.py
@@ -320,6 +320,70 @@ class QuantumCircuit3:
 
         print(f'qubit_freq_est_12 updated from {f0/self.GHz}GHz to {fit_params[0]/self.GHz}GHz.')
 
+    def calibrate_pi_amp_12(self, amps=None):
+
+        amp0 = self.pi_amp_12
+        if amps is None:
+            amps = np.linspace(0, 0.75, 75)
+
+        meas_idx = [self.qubit in group
+                        for group in self.backend_config.meas_map].index(True)
+
+        measure_pulse = self.backend_defaults.instruction_schedule_map.get(
+            'measure',
+            qubits = self.backend_config.meas_map[meas_idx],
+            )
+
+        pi_pulse_01 = pulse_lib.gaussian(duration=self.drive_samples,
+                                         amp=self.pi_amp_01,
+                                         sigma=self.drive_sigma,
+                                         name='pi_pulse_01')
+
+        schedules = []
+        for amp in amps:
+            base_pulse = pulse_lib.gaussian(duration=self.drive_samples,
+                                            sigma=self.drive_sigma,
+                                            amp=amp)
+
+            sidebanded_pulse = self.apply_sideband(base_pulse, self.qubit_freq_est_12)
+
+            # Define schedule
+            schedule = pulse.Schedule()
+            schedule |= pulse.Play(pi_pulse_01, self.drive_chan)
+            schedule |= pulse.Play(sidebanded_pulse, self.drive_chan) << schedule.duration
+            schedule |= measure_pulse << schedule.duration
+
+            # Accumulate
+            schedules.append(schedule)
+
+        job = self.backend.run(schedules,
+                               meas_level=1,
+                               meas_return='avg',
+                               shots=1024,
+                               schedule_los=[{self.drive_chan: self.qubit_freq_est_01}] * len(amps))
+
+        # Make notice about the on-going job
+        print("Calibrating pi_amp_12...")
+        job_monitor(job)
+
+        results = job.result(timeout=120)
+        results_data = [results.get_memory(i)[self.qubit] for i in range(len(results.results))]
+
+        # Fit response
+        fun = lambda x, period, a, b, c: a * np.cos( 2*np.pi*x/period - b ) + c
+        ydata = np.real(results_data)
+        # Ansatz
+        period = amps[-1]/2
+        a = ydata.max()-ydata.min()
+        b = 0.0
+        c = ydata.mean()
+        fit_params, *_ = curve_fit(fun, amps, ydata, [period, a, b, c])
+
+        # Update frequency estimate
+        self.pi_amp_12 = abs( (np.pi + fit_params[2]) * fit_params[0]/(2*np.pi) )
+
+        print(f'pi_amp_12 updated from {amp0} to {self.pi_amp_12}.')
+
     def compile(self):
         """
         Join all pulses and draw
