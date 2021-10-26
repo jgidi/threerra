b3eafe5315993cda8f860f2963bccbdeaa29ce0d
admunozmoller <68519346+admunozmoller@users.noreply.github.com>
2021-10-26 18:32:54 -0300

    added some documentation

--- threerra/pulses.py
+++ threerra/pulses.py
@@ -5,9 +5,23 @@ import numpy as np
 from qiskit import pulse
 import qiskit.pulse.library as pulse_lib
 
-def gen_pulse(qc3, shift_phase=0, angle=0, sideband_freq=0, name = None):
-    """Generates a pulse with given phase, angle and sideband frequency,
-    where the input phase must be in radians i.e (pi/s) s in [0, 2pi]"""
+def gen_pulse(qc3, shift_phase=0, angle=0, sideband_freq=0, name: Optional[str] = None):
+    """
+    Generates a pulse with given phase, angle and sideband frequency.
+    The parameters must be in radians i.e (pi/s), with s in [0, 2pi].
+
+        Args:
+            qc3: QuantumCircuit3 circuit.
+            shift_phase: The channel that will acquire data.
+            angle: The classical memory slot in which to store the classified readout result.
+            sideband_freq: The fast-access register slot in which to store the classified readout
+                      result for fast feedback.
+            name: Name of the pulse.
+
+        Returns:
+            Generated pulse with the desired parameters.
+            
+     """
 
     if name is None:
         name = ''
@@ -37,7 +51,16 @@ def gen_pulse(qc3, shift_phase=0, angle=0, sideband_freq=0, name = None):
 
 
 def measure(qc3):
-    """Measures the quantum circuit, collapsing it in each count to a specific value."""
+    """
+    Generates a measurement pulse on the given qubits, using the backend's default configuration.
+    
+        Args:
+            qc3: QuantumCircuit3 circuit.
+
+        Returns:
+            Generated measurement pulse.
+            
+    """
     meas_idx = [qc3.qubit in group
                        for group in qc3.backend_config.meas_map].index(True)
 
@@ -49,7 +72,15 @@ def measure(qc3):
     return pulse
 
 def pi_pulse_01(qc3):
+    """
+    Generates a 0 --> 1 pi pulse on the given qubits, using previously defined parameters for duration, pi amplitude, etc.
+    
+        Args:
+            qc3: QuantumCircuit3 circuit.
 
+        Returns:
+            Generated 0 --> 1 pi pulse.
+    """
     pulse = pulse_lib.gaussian(duration=qc3.drive_samples,
                                amp=qc3.pi_amp_01,
                                sigma=qc3.drive_sigma,
@@ -57,6 +88,16 @@ def pi_pulse_01(qc3):
     return pulse
 
 def pi_pulse_12(qc3):
+    """
+    Generates a 1 --> 2 pi pulse on the given qubits with the sideband method,
+    using the previously defined parameters for duration, pi amplitude, sideband frequency, etc.
+    
+        Args:
+            qc3: QuantumCircuit3 circuit.
+
+        Returns:
+            Generated 0--> 2 pi pulse.
+    """
 
     pulse = pulse_lib.gaussian(duration=qc3.drive_samples,
                                amp=qc3.pi_amp_12,
