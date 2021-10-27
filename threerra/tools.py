91f8b677f8dabf679f6277e78c0299027c13e86c
admunozmoller <68519346+admunozmoller@users.noreply.github.com>
2021-10-27 00:51:08 -0300

    added documentation

--- threerra/tools.py
+++ threerra/tools.py
@@ -14,6 +14,16 @@ def closest_multiple(N, base : int = 16):
 def get_data(result,
              discriminator=nearest_discriminator.discriminator,
              ):
+    """
+    Get the data of a job result using a user-defined discriminator
+    
+        Args:
+            result: Job's results from an experiment
+            discriminator: Nearest centroid or LDA discriminator.
+        
+        Returns:
+            Data of the job's results
+    """
 
     results_data = np.concatenate([result.get_memory(i).flatten()
                                    for i in range(len(result.results))])
@@ -24,6 +34,17 @@ def get_data(result,
 
 
 def get_counts(data, *args, **kwargs):
+    
+    """
+    Get the histogram data of an experiment.
+    
+        Args:
+            result: Job's results from an experiment or data obtained from a given job.
+        
+        Returns:
+            A dictionary that has the counts for each qubit.
+    """
+    
     # If data is a job result, use 'get_data' to get its data
     if isinstance(data, Result):
         data = get_data(data, *args, **kwargs)
